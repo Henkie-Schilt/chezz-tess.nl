ff35ce9ee90c8c85e348ad0c9949af0baf414eba
Henkie-Schilt <128805264+Henkie-Schilt@users.noreply.github.com>
2023-03-27 14:20:40 +0200

    Stap 7

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
     <main >
       <div >
         <p>
-          Hoi ik ben Chezz-Tess, en dit is mijn eerste pagina van de website.
+          Hoi ik ben de vriend van Chezz-Tess, en dit is de website die ik hier voor wil gaan gebruiken.
 
         </p>
       </div>
