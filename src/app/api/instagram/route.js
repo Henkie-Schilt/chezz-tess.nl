ba610cb0f971acb3ed7cb6d68d7b101c4346265b
Henkie-Schilt <128805264+Henkie-Schilt@users.noreply.github.com>
2023-03-31 21:44:23 +0200

    Test voor Werkende IG

--- src/app/api/instagram/route.js
+++ src/app/api/instagram/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 export const revalidate = 60 * 60 ; 
 export async function GET() {
    const mediaResponse = await fetch( 
-      `https://graph.instagram.com/${process.env.INSTAGRAM_USER_ID}/media?access_token=${process.env.INSTAGRAM_ACCESS_TOKEN}&fields=media_url,permalink,media_type` 
+      `https://graph.instagram.com/${process.env.INSTAGRAM_USER_ID}/media?access_token=${process.env.INSTAGRAM_ACCES_TOKEN}&fields=media_url,permalink,media_type` 
     ); 
       
     const media = await mediaResponse.json(); 
