4d0c9c2681a85a5b9a41fc10a785b0aeb9671cbd
Henkie-Schilt <128805264+Henkie-Schilt@users.noreply.github.com>
2023-04-01 00:26:58 +0200

    Werkende Email Handler?

--- src/components/organisms/Home.js
+++ src/components/organisms/Home.js
@@ -1,25 +1,33 @@
+
 "use client";
+import HandshakeOutlinedIcon from '@mui/icons-material/HandshakeOutlined';
+import InstagramTest from "@/molecules/InstagramTest";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import ProductCard from "@/molecules/ProductCard";
-import { HomeElement } from "@/config/elements";
+import Attention from "@/atoms/Attention";
 import products from "@/config/products";
-
+import Link from "next/link";
 const Home = () => (
-    <Grid2 container spacing={2} justifyContent="center">
+    <>
+      <Grid2 container spacing={2} justifyContent="center">
+        <Attention
+            text={
+                <span>
+                  
+                    Ik kan ook personelijke sierraden of kaarsen maken, je kunt hierover contact met me openemen via het {" "}
+                    <Link href="/contact" style={{ color: "black" }}>
+                        <b>contactpagina</b>
+                    </Link>
+                    .
+                </span>
+            }
+            icon={HandshakeOutlinedIcon}
+        />
         <Grid2 xs={12}>
-            <InstagramPosts />
+          <InstagramTest/>
         </Grid2>
-        {products.map(({ code }) => (
-            <Grid2 key={code} xs={12} sm={6} md={4} lg={3}>
-                <ProductCard code={code} imageHeight={380} />
-            </Grid2>
-        ))}
-    </Grid2>
-);
-
-export default Home;
-
-
-// <Grid2 xs={12} sx={{ mb: 2 }}>
-// {HomeElement}
-// </Grid2>
+      </Grid2>
+    </>
+  );
+  
+  export default Home;
