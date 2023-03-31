523e762612b6368a1e048460731ed13866eacc86
Henkie-Schilt <128805264+Henkie-Schilt@users.noreply.github.com>
2023-03-31 21:37:26 +0200

    Met de corecte IG code

--- src/app/api/instagram/route.js
+++ src/app/api/instagram/route.js
@@ -1,20 +1,12 @@
-import { AMOUNT_INSTAGRAM_POSTS } from "@/config/constants";
-import { NextResponse } from "next/server";
-
-export const revalidate = 1 * 60;
-
+import { NextResponse } from "next/server"; 
+export const revalidate = 60 * 60 ; 
 export async function GET() {
-    try {
-        const media = await fetch(
-            `https://graph.instagram.com/${process.env.INSTAGRAM_USER_ID}/media?access_token=${process.env.INSTAGRAM_ACCESS_TOKEN}&fields=media_url,permalink,media_type`
-        );
+   const mediaResponse = await fetch( 
+      `https://graph.instagram.com/${process.env.INSTAGRAM_USER_ID}/media?access_token=${process.env.INSTAGRAM_ACCESS_TOKEN}&fields=media_url,permalink,media_type` 
+    ); 
+      
+    const media = await mediaResponse.json(); 
+    return NextResponse.json(
+      media.data.filter(({ media_type }) => media_type = "IMAGE").slice(0, 6)); 
+  }
 
-        const posts = await media.json();
-        return NextResponse.json(
-            posts.data.filter(({ media_type }) => media_type === "IMAGE").slice(0, AMOUNT_INSTAGRAM_POSTS)
-        );
-    } catch (e) {
-        console.error(e);
-        return NextResponse.json([]);
-    }
-}
